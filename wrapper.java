0517e62382d68bbab50cb808489e1e3c23ef0399
poorna30646 <thotipoornachandra@gmail.com>
2024-12-12 11:30:07 +0530

    Add files via upload

---
+++ wrapper.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class fire{
+    public static void main(String args[]){
+        ArrayList<Integer> a= new ArrayList<>();
+        a.add(345);
+        a.add(234);
+        a.add(455);
+        System.out.println(a);
+    }
+}
