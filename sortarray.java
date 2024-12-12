4eab211ba31fe1c1d98de01cf26b921238d0cabf
poorna30646 <thotipoornachandra@gmail.com>
2024-12-12 12:19:31 +0530

    Add files via upload

---
+++ sortarray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class fire {
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(45);
+        a.add(1);
+        a.add(23);
+        a.add(78);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+}
